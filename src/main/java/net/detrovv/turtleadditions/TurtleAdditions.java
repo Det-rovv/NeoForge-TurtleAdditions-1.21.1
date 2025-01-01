2b3e0671024fcb5a36ce2f23c4685ccab887b1ff
Detrovv <erol.kerimov@protonmail.com>
2025-01-01 18:55:42 +0300

    "MODID" renamed to "MOD_ID"

--- src/main/java/net/detrovv/turtleadditions/TurtleAdditions.java
+++ src/main/java/net/detrovv/turtleadditions/TurtleAdditions.java
@@ -18,14 +18,15 @@ import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(TurtleAdditions.MODID)
+@Mod(TurtleAdditions.MOD_ID)
 public class TurtleAdditions
 {
-    public static final String MODID = "turtleadditions";
+    public static final String MOD_ID = "turtleadditions";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TurtleAdditions(IEventBus modEventBus, ModContainer modContainer)
     {
+        modEventBus.addListener(this::commonSetup)      ;
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
@@ -57,7 +58,7 @@ public class TurtleAdditions
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
         @SubscribeEvent
